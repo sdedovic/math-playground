67b900132536d8506c2e3eb82207bb06c0bddff0
sdedovic <stevan@dedovic.com>
2024-03-25 03:26:30 +0000

    init

---
+++ README.md
@@ -0,0 +1,11 @@
+# Maths Playground
+This is repo for me to work through some of my math books.
+
+## Starting
+```bash
+$ julia
+
+julia> import Pkg; Pkg.add("Pluto")
+
+julia> import Pluto; Pluto.run()
+```
---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    flake-utils,
+    nixpkgs,
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+        };
+      in {
+        formatter = pkgs.alejandra;
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            pkgs.julia
+          ];
+        };
+      }
+    );
+}
